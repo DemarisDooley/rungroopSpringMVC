52d5fcf9a07a65e3a957d66ab0c23edb4a377790
DemarisDooley <dooleydemaris@gmail.com>
2024-08-17 22:05:42 -0500

    2nd commmit

--- src/main/java/com/rungroop/web/WebApplication.java
+++ src/main/java/com/rungroop/web/WebApplication.java
@@ -2,12 +2,21 @@ package com.rungroop.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+@RestController
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
+//	@GetMapping
+//	public List<String> hi(){
+//		return List.of("Hello", "You","Person");
+//	}
 
 }
