910ae83363e41de1cebcc4547aa78b1bb6a8a9df
DemarisDooley <dooleydemaris@gmail.com>
2024-09-04 11:15:29 -0500

    Added Login, UserDetailsService, and createdBy

--- src/main/java/com/rungroop/web/service/UserService.java
+++ src/main/java/com/rungroop/web/service/UserService.java
@@ -1,7 +1,13 @@
 package com.rungroop.web.service;
 
 import com.rungroop.web.dto.RegistrationDto;
+import com.rungroop.web.models.UserEntity;
+import jakarta.validation.constraints.NotEmpty;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(@NotEmpty String email);
+
+    UserEntity findByUsername(@NotEmpty String username);
 }
